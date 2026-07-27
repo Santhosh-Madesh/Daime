30a9b34f85d49e29c216e493a2e529e431eb2f4d
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-07-27 22:11:15 +0530

    set up city model

---
+++ frameworks/models/cityModel.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+
+const citySchema = new mongoose.Schema({
+    name : { type: String, required:true, unique:true},
+},
+{
+    timestamps:true
+}
+)
+
+const cityModel = mongoose.model("City", citySchema);
+
+
+module.exports = cityModel;
