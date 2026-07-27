85115ec12217c678f62d403b79c01186a14d20ce
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-07-27 11:26:49 +0530

    add role field in the user schema

--- frameworks/models/userModel.js
+++ frameworks/models/userModel.js
@@ -4,7 +4,8 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
     name: { type: String, required: true},
     email: { type: String, required: true, unique: true},
-    password: { type: String, required: true}
+    password: { type: String, required: true},
+    role: { type: String, enum:["user", "org", "admin"], default: "user"}
 },
 {
     timestamps: true
