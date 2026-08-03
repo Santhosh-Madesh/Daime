68842b48702ac67ee0bed04e92ef5dd7ce1ac8d3
Santhosh-Madesh <santoshmadesh05@gmail.com>
2026-08-03 12:40:40 +0530

    write tests for create show use-case

---
+++ domain/useCases/show/createShow.js
@@ -0,0 +1,63 @@
+
+class CreateShow{
+
+    constructor({ showRepository, movieRepository, screenRepository }){
+        this.showRepository = showRepository;
+        this.movieRepository = movieRepository;
+        this.screenRepository = screenRepository;
+    }
+
+    async execute(show){
+
+        if(!show || !show.startsAt || !show.endsAt || !show.screenId || !show.movieId ){ return false }
+
+        if(show.startsAt.getTime() >= show.endsAt.getTime()){ return false }
+
+        const movie = await this.movieRepository.findById(show.movieId);
+        if(!movie){ return false }
+
+        const movieDuration = movie.duration;
+        const showDuration = (show.endsAt - show.startsAt)/3600000;
+
+        if(showDuration !== movieDuration){ return false }
+
+        const screen = await this.screenRepository.findById(show.screenId);
+        if(!screen){ return false }
+
+
+        const showsOfScreen = await this.showRepository.findByScreenId(show.screenId);
+
+        for( const oneShow of showsOfScreen ){
+
+            if(oneShow.starts_at.getDate() != show.startsAt.getDate()){
+                continue
+            }
+
+            const showStarts = show.startsAt.getHours();
+            const showEnds = show.endsAt.getHours();
+
+            if(showStarts >= oneShow.starts_at.getHours() && showStarts <= oneShow.ends_at.getHours()){ return false }
+            if(showEnds >= oneShow.starts_at.getHours() && showEnds <= oneShow.ends_at.getHours()){ return false }
+
+            if(oneShow.starts_at.getHours() >= showStarts && oneShow.starts_at.getHours() <= showEnds){ return false }
+            if(oneShow.ends_at.getHours() >= showStarts && oneShow.ends_at.getHours() <= showEnds){ return false }
+
+        }
+
+        const showCreated = await this.showRepository.create(show);
+
+        const result = {
+            startsAt: showCreated.starts_at,
+            endsAt: showCreated.ends_at,
+            screenId: showCreated.screen_id,
+            movieId: showCreated.movie_id
+        }
+
+        return result
+    }
+
+
+}
+
+
+module.exports = CreateShow;
---
+++ tests/showTests/createShow.test.js
@@ -0,0 +1,190 @@
+const MockShowRepository = require("../mockRepositories/mockShowRepository");
+const MockMovieRepository = require("../mockRepositories/mockMovieRepository");
+const MockScreenRepository = require("../mockRepositories/mockScreenRepository");
+
+const CreateShow = require("../../domain/useCases/show/createShow");
+
+const mockShowRepository = new MockShowRepository;
+const mockMovieRepository = new MockMovieRepository;
+const mockScreenRepository = new MockScreenRepository;
+
+const createShowUseCase = new CreateShow({ showRepository: mockShowRepository, movieRepository: mockMovieRepository, screenRepository: mockScreenRepository });
+
+beforeEach(()=>{
+    mockShowRepository.clearSpyCalls();
+    mockShowRepository.resetData();
+})
+
+test("when no inputs provided, return false", async()=>{
+
+    const payloads = ["", null, undefined, {}];
+
+    for( const payload of  payloads ){
+
+        const showCreated = await createShowUseCase.execute(payload);
+
+        expect(showCreated).toBe(false);
+
+    }
+})
+
+test("when not all necessary inputs are provided, return false", async()=>{
+
+    const payloads =[
+        {
+            startsAt: new Date("2026-08-05T09:00:00Z"),
+            screenId: "FFAA33F",
+            movieId :"FFAA33F"
+        },
+        {
+            endsAt: new Date("2026-08-05T12:00:00Z"),
+            screenId: "FFAA33F",
+            movieId :"FFAA33"
+        },
+        {
+            startsAt: new Date("2026-08-05T09:00:00Z"),
+            endsAt: new Date("2026-08-05T12:00:00Z"),
+            screenId: "FFAA33F",
+        },
+        {
+            startsAt: new Date("2026-08-05T09:00:00Z"),
+            endsAt: new Date("2026-08-05T12:00:00Z"),
+            movieId :"FFAA33"
+        }
+    ]
+
+    for( const payload of payloads ){
+
+        const showCreated = await createShowUseCase.execute(payload);
+
+        expect(showCreated).toBe(false);
+    }
+})
+
+test("when provided endsAt is before startAt including both date and time, return false", async()=>{
+
+    const payload = {
+        startsAt: new Date("2026-08-05T09:00:00Z"),
+        endsAt: new Date("2026-08-05T08:00:00Z"),
+        screenId: "FFAA33F",
+        movieId :"FFAA33F"
+    }
+
+    const showCreated = await createShowUseCase.execute(payload);
+
+    expect(showCreated).toBe(false)
+})
+
+test("when provided a screen ID that does not exists, return false", async()=>{
+
+    const payload = {
+        startsAt: new Date("2026-08-05T09:00:00Z"),
+        endsAt: new Date("2026-08-05T12:00:00Z"),
+        screenId: "INVALIDSCREENID",
+        movieId :"FFAA33F"
+    }
+
+    const showCreated = await createShowUseCase.execute(payload);
+
+    expect(showCreated).toBe(false)
+})
+
+test("when provided a movie ID that does not exists, return false", async()=>{
+
+    const payload =  {
+        startsAt: new Date("2026-08-05T09:00:00Z"),
+        endsAt: new Date("2026-08-05T12:00:00Z"),
+        screenId: "FFAA33F",
+        movieId :"INVALIDMOVIEID"
+    }
+
+    const showCreated = await createShowUseCase.execute(payload);
+
+    expect(showCreated).toBe(false)
+})
+
+test("when the movie duration does not match the start and end duration, return false", async()=>{
+
+    const payload =  {
+        startsAt: new Date("2026-08-05T09:00:00Z"),
+        endsAt: new Date("2026-08-05T11:30:00Z"),
+        screenId: "FFAA33F",
+        movieId :"FFAA33F"
+    }
+
+    const showCreated = await createShowUseCase.execute(payload);
+
+    expect(showCreated).toBe(false)
+
+})
+
+test("if there exist another show at the same screen that is in between the current start and end time, return false", async()=>{
+
+    const payloads = [
+        
+        {
+            startsAt: new Date("2026-08-03T09:00:00Z"),
+            endsAt: new Date("2026-08-03T11:30:00Z"),
+            screenId: "AEFEEA",
+            movieId: "BBCDDFEE"
+        },
+        {
+            startsAt: new Date("2026-08-03T09:30:00Z"),
+            endsAt: new Date("2026-08-03T12:00:00Z"),
+            screenId: "AEFEEA",
+            movieId: "BBCDDFEE"
+        },
+        {
+            startsAt: new Date("2026-08-03T08:30:00Z"),
+            endsAt: new Date("2026-08-03T11:00:00Z"),
+            screenId: "AEFEEA",
+            movieId: "BBCDDFEE"
+        },
+    ]
+
+    for( const payload of payloads ){
+
+        const showCreated = await createShowUseCase.execute(payload);
+        expect(showCreated).toBe(false)
+
+    }
+    
+
+    
+    
+})
+
+test("when provided valid data, call the create method of show repo", async()=>{
+
+    const payload = {
+        startsAt: new Date("2026-08-05T09:30:00Z"),
+        endsAt: new Date("2026-08-05T12:00:00Z"),
+        screenId: "AEFEEA",
+        movieId: "BBCDDFEE"
+    }
+
+    const showCreated = await createShowUseCase.execute(payload);
+
+    expect(mockShowRepository.createSpyCall).toBe(1)
+})
+
+test("when provided a valid data, return the payload", async()=>{
+
+    const payload = {
+        startsAt: new Date("2026-08-05T09:00:00Z"),
+        endsAt: new Date("2026-08-05T12:00:00Z"),
+        screenId: "FFAA33F",
+        movieId :"FFAA33F"
+    }
+
+    const showCreated = await createShowUseCase.execute(payload);
+
+    expect(showCreated).toStrictEqual({
+        startsAt: new Date("2026-08-05T09:00:00Z"),
+        endsAt: new Date("2026-08-05T12:00:00Z"),
+        screenId: "FFAA33F",
+        movieId :"FFAA33F"
+    })
+})
+
+// Next test: if the year doesnt match na return false
